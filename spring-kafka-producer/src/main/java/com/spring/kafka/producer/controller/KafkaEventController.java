0f75ba1083bd2a200f8db0a21564db69aaddaf41
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2024-06-30 22:23:21 +0530

    Included the project for spring kafka producer

---
+++ spring-kafka-producer/src/main/java/com/spring/kafka/producer/controller/KafkaEventController.java
@@ -0,0 +1,31 @@
+package com.spring.kafka.producer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.kafka.producer.service.KafkaMessagePublisher;
+
+@RestController
+@RequestMapping("/producer-app")
+public class KafkaEventController {
+
+	@Autowired
+	private KafkaMessagePublisher messagePublisher;
+	
+	@GetMapping("/publish/{message}")
+	public ResponseEntity<?> publishMessage(@PathVariable String message) {
+		try {
+			messagePublisher.sendMessageToTopic(message);
+			return ResponseEntity.ok("Message published successfully !!");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();		
+		}
+	}
+}
