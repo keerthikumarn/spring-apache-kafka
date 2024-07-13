0f446e56743a9f8e9c5b31c8ac7940dfe153c8f3
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2024-07-13 16:52:31 +0530

    included testcontainers dependencies and cleanup in Employee class

--- spring-kafka-consumer/src/main/java/com/spring/kafka/dto/Employee.java
+++ spring-kafka-consumer/src/main/java/com/spring/kafka/dto/Employee.java
@@ -1,10 +1,12 @@
 package com.spring.kafka.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
 
 @Data
 @ToString
+@AllArgsConstructor
 public class Employee {
 	
 	private int id;
